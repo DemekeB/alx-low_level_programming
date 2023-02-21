1a2ad62aa84eb9a6bb3ee5bc06a6fefd57bc8cbf
DemekeB <birukdemeke24@gmail.com>
2023-02-21 05:30:24 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -32,8 +32,8 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d", n);
-                                printf("\n");
-                                break;
+				printf("\n");
+				break;
 			}
 			else
 			{
