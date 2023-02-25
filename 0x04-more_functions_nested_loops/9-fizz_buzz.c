93ecde922528787c16b14bafd900d07cbc4bcbe7
DemekeB <birukdemeke24@gmail.com>
2023-02-24 18:08:51 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int main(void)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0))
 		{
-			 printf("FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (x % 5 == 0);
+		else if (x % 5 == 0)
 		{
 			printf("Buzz");
 		}
@@ -34,3 +34,6 @@ int main(void)
 	printf("\n");
 	return (0);
 }
+
+
+
